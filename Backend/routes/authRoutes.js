9eea6dba8898be0528b82a9b2be9c3ac236545a5
starfruit220004 <hannahjeanbalimbingan@gmail.com>
2026-05-04 06:43:39 +0800

    port fix

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -10,10 +10,10 @@ const router = express.Router();
 
 // Nodemailer transporter
 const transporter = nodemailer.createTransport({
+  service: "gmail",
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, 
-  family: 4,
+  port: 465,
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
