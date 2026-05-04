d72d8237d30dda2b5d9ff8ff017639ff21449d96
starfruit220004 <hannahjeanbalimbingan@gmail.com>
2026-05-04 21:58:57 +0800

    brevo

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,18 +1,16 @@
-// utils/sendEmail.js
 const nodemailer = require('nodemailer');
 const dns = require('dns');
 
-// Force IPv4 to avoid IPv6 issues on Render
 dns.setDefaultResultOrder('ipv4first');
 
 const sendEmail = async ({ email, subject, html }) => {
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    host: 'smtp-relay.brevo.com',
+    port: 587,
+    secure: false,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
     },
     family: 4,
   });
