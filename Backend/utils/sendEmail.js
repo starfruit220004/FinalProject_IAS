0470c8cdc09adc8d9d1e6ea000986e3cb677e1b4
starfruit220004 <hannahjeanbalimbingan@gmail.com>
2026-05-04 21:35:38 +0800

    port

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -8,8 +8,8 @@ dns.setDefaultResultOrder('ipv4first');
 const sendEmail = async ({ email, subject, html }) => {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
